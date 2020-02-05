988a9e6d621a801968dabe872ec6c7ea6c853565
Leem437 <salimmaxwell@gmail.com>
2020-02-04 19:38:45 -0500

    A couple pages and majority of code behind

--- drugs/drugs_search.ascx.cs
+++ drugs/drugs_search.ascx.cs
@@ -12,36 +12,241 @@ namespace FinalProject.drugs
 {
 	public partial class drugs_search : UCPageType
 	{
+		// Create instance of gridview code and encryption class
+		gridviewcode grd = new gridviewcode();
 		public encryption cipher = new encryption();
 
 		protected void Page_Load(object sender, EventArgs e)
 		{
+			// Put previously typed values back into text boxes, if any (used on return from vieweditadd page)
+			txtSrchDrugID.Text = Convert.ToString(Session["srchDrugID"]);	//CHANGED
+			txtSrchDrugName.Text = Convert.ToString(Session["srchDrugName"]);	//CHANGED
+			txtSrchDrugDesc.Text = Convert.ToString(Session["srchDrugDesc"]);	//CHANGED
 
+			// Establish GridView event handlers
+			grdDrugs.PageIndexChanging += new GridViewPageEventHandler(grdDrugs_PageIndexChanging); //CHANGED
+			grdDrugs.Sorting += new GridViewSortEventHandler(grdDrugs_Sorting); //CHANGED
+
+			if (Convert.ToString(Session["srchPatHasSearched"]) == "true") // Check if search already performed
+			{
+				// Repopulate GridView
+				PopulateGridView();
+			}
+
+			// Keep delete confirmation loaded on partial postback, if already loaded
+			if (Convert.ToString(Session["delConfirmActive"]) == "true")
+			{
+				switch (Convert.ToString(Session["delConfirmType"])) // Check whether single or multi delete
+				{
+					case "single":
+						// Load single delete confirmation
+						Delete_Click(sender, new CommandEventArgs("delete", Convert.ToString(Session["delDrugID"])));
+						break;
+					case "multi":
+						// Load multi delete confirmation
+						btnDeleteChecked_Click(sender, e);
+						break;
+				}
+			}
 		}
 
-		protected void btnDeleteChecked_Click(object sender, EventArgs e)
-		{
-
-		}
 
 		protected void btnSearch_Click(object sender, EventArgs e)
 		{
+			try
+			{
+				// Save values in text boxes to session
+				SrchDrugSaveSession();	//CHANGED
 
+				// Populate GridView
+				PopulateGridView();
+			}
+			catch
+			{
+				// Display failure message
+				RegisterAlertScript(new CommandEventArgs("script", "Failed to load patient data"));
+			}
+		}
+
+		protected void PopulateGridView()
+		{
+			// Get text box session values
+			string DrugID = Convert.ToString(Session["srchDrugID"]);    //CHANGED
+			string DrugName = Convert.ToString(Session["srchDrugName"]);    //CHANGED
+			string DrugDesc = Convert.ToString(Session["srchDrugDesc"]);    //CHANGED
+
+			// Initiate data tier
+			LouisDataTier aDrug = new LouisDataTier();  //CHANGED
+			DataSet DrugData = new DataSet();   //CHANGED
+			DrugData = aDrug.SearchDrugs(DrugID, DrugName, DrugDesc);   //CHANGED
+
+			// Populate datagrid with dataset
+			grdDrugs.DataSource = DrugData.Tables[0];   //CHANGED
+			grdDrugs.DataBind();    //CHANGED
+
+			// Show/Enable Delete Selected button if table has rows
+			if (grdDrugs.Rows.Count > 0)    //CHANGED
+			{
+				btnDeleteChecked.Enabled = true;
+				btnDeleteChecked.Visible = true;
+			}
+			else // Table empty, hide Delete Selected button
+			{
+				btnDeleteChecked.Enabled = false;
+				btnDeleteChecked.Visible = false;
+			}
+
+			// Set value that patient search has been performed
+			Session["srchDrugHasSearched"] = "true";    //CHANGED
+
+			// Delete any cached GridView data
+			Cache.Remove("srchDrugGridDataView");	//CHANGED
+			// Add new data to cache
+			Cache.Add("srchDrugGridDataView", new DataView(DrugData.Tables[0]), //CHANGED
+				null, System.Web.Caching.Cache.NoAbsoluteExpiration, System.TimeSpan.FromMinutes(10),
+				System.Web.Caching.CacheItemPriority.Default, null);
 		}
 
 		public void View_Click(object sender, CommandEventArgs e)
 		{
-
+			// Trigger view button click event for default page
+			ViewButtonClicked(e);
 		}
 
 		public void Edit_Click(object sender, CommandEventArgs e)
 		{
-
+			// Trigger edit button click event for default page
+			EditButtonClicked(e);
 		}
 
 		public void Delete_Click(object sender, CommandEventArgs e)
 		{
+			// Loads delete confirmation control
+			Session["delDrugID"] = e.CommandArgument.ToString();    //CHANGED                        // Store patient ID of record to be deleted
+			deleteconfirm ucDelConfirm = (deleteconfirm)LoadControl("/deleteconfirm.ascx"); // Load delete confirmation control
+			ucDelConfirm.ID = "ucDelConfirm";
+			ucDelConfirm.DeleteType = "single";                                             // Set deletion type to single
+			this.pnlDeleteConfirm.ContentTemplateContainer.Controls.Clear();                // Clear content of delete confirmation update panel, if any
+			this.pnlDeleteConfirm.ContentTemplateContainer.Controls.Add(ucDelConfirm);      // Add delete confirmation control to update panel
 
+			// Assign event handlers
+			ucDelConfirm.confirmed += new CommandEventHandler(DeleteConfirmed);
+			ucDelConfirm.cancelled += new Action(RemoveDeleteConfirmation);
 		}
+
+		public void btnDeleteChecked_Click(object sender, EventArgs e)
+		{
+			// Loads delete confirmation control
+			deleteconfirm ucDelConfirm = (deleteconfirm)LoadControl("/deleteconfirm.ascx"); // Load delete confirmation control
+			ucDelConfirm.ID = "ucDelConfirm";
+			ucDelConfirm.DeleteType = "multi";                                              // Set deletion type to multiple
+			this.pnlDeleteConfirm.ContentTemplateContainer.Controls.Clear();                // Clear content of delete confirmation update panel, if any
+			this.pnlDeleteConfirm.ContentTemplateContainer.Controls.Add(ucDelConfirm);      // Add delete confirmation control to update panel
+
+			// Assign event handlers
+			ucDelConfirm.confirmed += new CommandEventHandler(DeleteConfirmed);
+			ucDelConfirm.cancelled += new Action(RemoveDeleteConfirmation);
+		}
+
+		public void DelRegisterAlertScript(object sender, CommandEventArgs e)
+		{
+			// Pass alert script event from delete confirmation control to main page
+			RegisterAlertScript(e);
+		}
+
+		public void DeleteConfirmed(object sender, CommandEventArgs e)
+		{
+			// Initiate data tier and variables
+			LouisDataTier dataTier = new LouisDataTier();
+			bool deleteSuccess = false;
+			string plural = "";
+
+			// Check delete type
+			switch (e.CommandArgument.ToString())
+			{
+				case "single": // Deleting a single record
+					string singleDrugID = cipher.Decrypt(Convert.ToString(Session["delDrugID"]));   //CHANGED 
+					deleteSuccess = dataTier.DeletePatient(singleDrugID);   //CHANGED 
+					break;
+
+				case "multi": // Deleting all checked records
+					plural = "s";
+					if (grdDrugs.Rows.Count > 0)
+					{
+						// Loop through rows
+						foreach (GridViewRow row in grdDrugs.Rows)
+						{
+							// Find checkbox of row
+							CheckBox chkSelectDrugID = new CheckBox();  //CHANGED 
+							chkSelectDrugID = (CheckBox)row.FindControl("chkDrugID");   //CHANGED 
+
+							// Delete record if checkbox checked
+							if (chkSelectDrugID.Checked)    //CHANGED 
+							{
+								string multiDrugID = row.Cells[1].Text; //CHANGED 
+								deleteSuccess = dataTier.DeletePatient(multiDrugID);    //CHANGED 
+							}
+						}
+					}
+					break;
+			}
+
+			if (deleteSuccess == true)
+			{
+				// Display success message
+				RegisterAlertScript(new CommandEventArgs("script", "Drug record" + plural + " deleted successfully"));  //CHANGED 
+			}
+			else
+			{
+				// Display failure message
+				RegisterAlertScript(new CommandEventArgs("script", "Failed to delete drug record" + plural));   //CHANGED 
+			}
+
+			// Remove delete confirmation control and update GridView
+			RemoveDeleteConfirmation();
+			PopulateGridView();
+		}
+
+		public void RemoveDeleteConfirmation()
+		{
+			// Remove delete confirmation control
+			pnlDeleteConfirm.ContentTemplateContainer.Controls.Clear();
+			Session["delConfirmActive"] = false;
+		}
+
+		protected void grdDrugs_PageIndexChanging(object sender, GridViewPageEventArgs e)   //CHANGED 
+		{
+			int pageNum = e.NewPageIndex;
+			Paging(pageNum);
+		}
+
+		private void Paging(int page)
+		{
+			grdDrugs.PageIndex = page;  //CHANGED 
+			PopulateGridView();
+		}
+
+		protected void grdDrugs_Sorting(object sender, GridViewSortEventArgs e)
+		{
+			string newSortExpr = e.SortExpression;
+			string sortDir = (string)Session["srchDrugSortDir"];    //CHANGED 
+			string oldSortExpr = (string)Session["srchDrugSortExpr"];   //CHANGED 
+			DataView DrugData = (DataView)Cache["srchDrugGridDataView"];    //CHANGED 
+
+			var sortedDrugData = grd.SortRecords(oldSortExpr, newSortExpr, sortDir, DrugData);
+			grdDrugs.DataSource = sortedDrugData.Item1;
+			Session["srchDrugSortDir"] = sortedDrugData.Item2;
+			Session["srchDrugSortExpr"] = newSortExpr;
+			grdDrugs.DataBind();
+		}
+
+		public void SrchDrugSaveSession()
+		{
+			// Save currently entered text box values to session
+			Session["srchDrugID"] = txtSrchDrugID.Text;
+			Session["srchDrugName"] = txtSrchDrugName.Text;
+			Session["srchDrugDesc"] = txtSrchDrugDesc.Text;
+		}
+
 	}
 }
--- drugs/drugs_vieweditadd.ascx.cs
+++ drugs/drugs_vieweditadd.ascx.cs
@@ -9,21 +9,184 @@ namespace FinalProject.drugs
 {
 	public partial class drugs_vieweditadd : UCPageType
 	{
+		// Create instance of encryption class
 		public encryption cipher = new encryption();
 
 		protected void Page_Load(object sender, EventArgs e)
 		{
 
+			// Decrypt and store patient ID if included
+			string DrugID = "";
+			if (!string.IsNullOrEmpty(PrimaryKey))
+			{
+				DrugID = cipher.Decrypt(PrimaryKey);
+			}
+
+			// Do different things for each page type
+			switch (PageType)
+			{
+				case "view":
+					// Populate controls
+					PopulateForms(DrugID);
+					// Disable all controls
+					DisableAllControls();
+					// Set header text
+					lblPageHeader.Text = "View Drug Record";
+					break;
+
+				case "edit":
+					// Populate controls
+					PopulateForms(DrugID);
+					// Enable patient ID text box
+					txtDrugID.Enabled = false;
+					// Set header text
+					lblPageHeader.Text = "Edit Drug Record";
+					break;
+
+				case "add":
+					// Set header text
+					lblPageHeader.Text = "Add Drug Record";
+					break;
+
+				default: // Invalid PageType
+					Response.Redirect("/drugs");
+					break;
+			}
 		}
 
-		protected void btnGoBack_Click(object sender, EventArgs e)
+		protected void PopulateForms(string DrugID)
 		{
+			try
+			{
+				// Populate Patient ID textbox with input value
+				txtDrugID.Text = DrugID;
 
+				// Execute stored procedure
+				LouisDataTier dataTier = new LouisDataTier();
+				//DataSet ds = new DataSet();
+				DataSet DrugData = dataTier.GetPatient(DrugID);
+
+				if (DrugData.Tables[0].Rows.Count > 0) // If anything is returned
+				{
+					// Populate text/combo boxes with values
+					txtDrugID.Text = DrugData.Tables[0].Rows[0]["DrugID"].ToString();
+					txtDrugName.Text = DrugData.Tables[0].Rows[0]["DrugName"].ToString();
+					txtDrugDesc.Text = DrugData.Tables[0].Rows[0]["DrugDesc"].ToString();
+					txtMethodOfAdmin.Text = DrugData.Tables[0].Rows[0]["methodOfAdmin"].ToString();
+				}
+			}
+			catch
+			{
+				// Display failure message
+				RegisterAlertScript(new CommandEventArgs("script", "Failed to populate controls"));
+			}
+		}
+
+		protected void DisableAllControls()
+		{
+			// Disable text boxes and hide buttons
+			txtDrugID.Enabled = false;
+			txtDrugName.Enabled = false;
+			txtDrugDesc.Enabled = false;
+			txtMethodOfAdmin.Enabled = false;
+			btnSubmit.Enabled = false;
+			btnSubmit.Visible = false;
 		}
 
 		protected void btnSubmit_Click(object sender, EventArgs e)
 		{
+			// Retrieve page type
+			string pageType = PageType;
 
+			// Check for invalid null values and show error message
+			StringBuilder emptyMessage = new StringBuilder();
+			emptyMessage.Append("The following fields cannot be empty:" + "\\n"); // Double backslashes are required for registering JS to work
+			bool emptyFields = false;
+
+			if (txtDrugID.Text.Trim() == string.Empty)
+			{
+				emptyMessage.Append("Drug ID" + "\\n");
+				emptyFields = true;
+			}
+			if (txtDrugName.Text.Trim() == string.Empty)
+			{
+				emptyMessage.Append("Drug Name" + "\\n");
+				emptyFields = true;
+			}
+			if (txtDrugDesc.Text.Trim() == string.Empty)
+			{
+				emptyMessage.Append("Drug Description" + "\\n");
+				emptyFields = true;
+			}
+			if (txtMethodOfAdmin.Text.Trim() == string.Empty)
+			{
+				emptyMessage.Append("Method Of Administration" + "\\n");
+				emptyFields = true;
+			}
+			if (emptyFields == true)
+			{
+				RegisterAlertScript(new CommandEventArgs("script", emptyMessage.ToString()));
+				return;
+			}
+
+			// Initiate data tier
+			LouisDataTier dataTier = new LouisDataTier();
+
+			// Determine editing or adding record
+			switch (pageType)
+			{
+				case "edit":
+					// Execute stored procedure (returns true if successful)
+					bool updateSuccess = dataTier.UpdateDrug(
+						txtDrugID.Text.Trim(),
+						txtDrugName.Text.Trim(),
+						txtDrugDesc.Text.Trim(),
+						txtMethodOfAdmin.Text.Trim()
+						);
+
+					if (updateSuccess == true)
+					{
+						// Display success message
+						RegisterAlertScript(new CommandEventArgs("script", "Drug record updated successfully"));
+					}
+					else
+					{
+						// Display failure message
+						RegisterAlertScript(new CommandEventArgs("script", "Failed to update drug record"));
+					}
+					break;
+
+				case "add":
+					// Execute stored procedure (returns true if successful)
+					bool addSuccess = dataTier.AddDrug(
+						txtDrugID.Text.Trim(),
+						txtDrugName.Text.Trim(),
+						txtDrugDesc.Text.Trim(),
+						txtMethodOfAdmin.Text.Trim()
+						);
+
+					if (addSuccess == true)
+					{
+						// Display success message
+						RegisterAlertScript(new CommandEventArgs("script", "Drug record added successfully"));
+					}
+					else
+					{
+						// Display failure message
+						RegisterAlertScript(new CommandEventArgs("script", "Failed to add Drug record"));
+					}
+					break;
+				default:
+					// Display error message
+					RegisterAlertScript(new CommandEventArgs("script", "Invalid pageType"));
+					break;
+			}
+		}
+
+		protected void btnGoBack_Click(object sender, EventArgs e)
+		{
+			// Trigger event to load search page
+			GoBackButtonClicked(e);
 		}
 	}
 }
